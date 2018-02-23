6df2209cac158123937b39e461c2dbeb20ca1300
Michael Schulz <mschulz5991@gmail.com>
2018-02-22 20:18:47 -0800

    Implemented tests for add at pos and delete front/back

--- tests/inc/test_list.h
+++ tests/inc/test_list.h
@@ -3,6 +3,8 @@
 
 void test_list_new_int();
 
+void test_list_free();
+
 void test_list_add_to_front_empty_int();
 
 void test_list_get_length();
@@ -13,11 +15,24 @@ void test_list_add_to_back_empty_int();
 
 void test_list_add_to_back_non_empty_int();
 
+void test_list_add_to_pos_zero_empty_int();
+
+void test_list_add_to_pos_length_empty_int();
+
+void test_list_add_to_pos_non_empty_int();
+
+
+void test_list_delete_from_front_to_empty_int();
+void test_list_delete_from_front_to_non_empty_int();
+
+void test_list_delete_from_back_to_empty_int();
+void test_list_delete_from_back_to_non_empty_int();
+
+
+
 void test_list_new_str();
 
 void test_list_add_to_front_empty_str();
-
-void test_list_ptr_not_same();
 
 void test_all_list_scenarios();
 
--- tests/src/test_list.c
+++ tests/src/test_list.c
@@ -5,6 +5,10 @@
 #include <assert.h>
 #include <string.h>
 
+#define AMOUNT_TO_ADD 10
+#define VALUE_TO_ADD AMOUNT_TO_ADD+1
+#define POS_TO_ADD AMOUNT_TO_ADD/2
+
 void test_list_new_int(){
 	list list;
 
@@ -21,12 +25,25 @@ void test_list_new_int(){
 	printf("Test list new int SUCCEEDED\n");
 }
 
+void test_list_free(){
+	list list;
+
+	int toAdd = VALUE_TO_ADD;
+
+	list_new(&list, sizeof(int), NULL);
+	list_add_to_front(&list, &toAdd);
+	list_free(&list);
+
+	assert(list.front == NULL);
+	assert(list.back == NULL);
+}
+
 void test_list_add_to_front_empty_int(){
 	list list;
 
 	int toAdd, frontValue, backValue, posValue;
 
-	toAdd=5;
+	toAdd=VALUE_TO_ADD;
 
 	list_new(&list, sizeof(int), NULL);
 
@@ -52,7 +69,7 @@ void test_list_get_length()
 
 	int toAdd;
 
-	toAdd=5;
+	toAdd=VALUE_TO_ADD;
 
 	list_new(&list, sizeof(int), NULL);
 
@@ -71,8 +88,8 @@ void test_list_add_to_front_non_empty_int(){
 	list list;
 	int valueToAdd, i, frontValue, backValue, posValue, numberToAdd;
 
-	valueToAdd = 32;
-	numberToAdd = 10;
+	valueToAdd = VALUE_TO_ADD;
+	numberToAdd = AMOUNT_TO_ADD;
 
 	list_new(&list, sizeof(int), NULL);
 
@@ -105,7 +122,7 @@ void test_list_add_to_back_empty_int(){
 
 	int toAdd, frontValue, backValue, posValue;
 
-	toAdd=5;
+	toAdd=VALUE_TO_ADD;
 
 	list_new(&list, sizeof(int), NULL);
 
@@ -131,8 +148,8 @@ void test_list_add_to_back_non_empty_int(){
 	list list;
 	int valueToAdd, i, frontValue, backValue, posValue, numberToAdd;
 
-	valueToAdd = 32;
-	numberToAdd = 10;
+	valueToAdd = VALUE_TO_ADD;
+	numberToAdd = AMOUNT_TO_ADD;
 
 	list_new(&list, sizeof(int), NULL);
 
@@ -158,6 +175,189 @@ void test_list_add_to_back_non_empty_int(){
 
 	printf("Test list add to back from non empty int SUCCEEDED\n");
 
+}
+
+void test_list_add_to_pos_zero_empty_int(){
+	list list;
+
+	int toAdd, frontValue, backValue, posValue;
+
+	toAdd=VALUE_TO_ADD;
+
+	list_new(&list, sizeof(int), NULL);
+
+	list_add_to_position(&list, &toAdd, 0);
+
+	list_get_front(&list, &frontValue);
+	list_get_back(&list, &backValue);
+	list_get_at_position(&list, &posValue, 0);
+
+	assert(frontValue == toAdd);
+	assert(backValue == toAdd);
+	assert(posValue == toAdd);
+	assert(list_get_length(&list) == 1);
+
+	list_free(&list);
+
+	printf("Test list add to pos from empty to zero int SUCCEEDED\n");	
+}
+
+void test_list_add_to_pos_length_empty_int(){
+	list list;
+
+	int toAdd, position, frontValue, backValue, posValue;
+
+	toAdd=VALUE_TO_ADD;
+
+	list_new(&list, sizeof(int), NULL);
+
+	position = list_get_length(&list);
+
+	list_add_to_position(&list, &toAdd, position);
+
+	list_get_front(&list, &frontValue);
+	list_get_back(&list, &backValue);
+	list_get_at_position(&list, &posValue, position);
+
+	assert(frontValue == toAdd);
+	assert(backValue == toAdd);
+	assert(posValue == toAdd);
+	assert(list_get_length(&list) == 1);
+
+	list_free(&list);
+
+	printf("Test list add to pos from empty to length int SUCCEEDED\n");
+}
+
+void test_list_add_to_pos_non_empty_int(){
+	list list;
+	int valueToAdd, i, position, frontValue, backValue, posValue, numberToAdd;
+
+	valueToAdd = VALUE_TO_ADD;
+	numberToAdd = AMOUNT_TO_ADD;
+	position = POS_TO_ADD;
+
+	list_new(&list, sizeof(int), NULL);
+
+	for(i=0;i<numberToAdd;i++)
+	{
+		list_add_to_front(&list, &i);
+	}
+
+	numberToAdd++;
+
+	list_add_to_position(&list, &valueToAdd, position);
+
+	list_get_front(&list, &frontValue);
+	list_get_back(&list, &backValue);
+	list_get_at_position(&list, &posValue, position);
+
+	assert(posValue == valueToAdd);
+	assert(list_get_length(&list) == numberToAdd);
+
+	list_free(&list);
+
+	printf("Test list add to pos from non empty int SUCCEEDED\n");
+}
+
+
+void test_list_delete_from_front_to_empty_int(){
+
+	list list;
+
+	int toAdd;
+
+	toAdd=VALUE_TO_ADD;
+
+	list_new(&list, sizeof(int), NULL);
+
+	list_add_to_front(&list, &toAdd);
+
+	list_delete_from_front(&list);
+
+	assert(list_get_length(&list) == 0);
+	assert(list.front == NULL);
+	assert(list.back == NULL);
+
+	list_free(&list);
+
+	printf("Test list delete from front to empty int SUCCEEDED\n");
+}
+
+void test_list_delete_from_front_to_non_empty_int(){
+	
+	list list;
+	int valueToAdd, i, frontValue, numberToAdd;
+
+	valueToAdd = VALUE_TO_ADD;
+	numberToAdd = AMOUNT_TO_ADD;
+
+	list_new(&list, sizeof(int), NULL);
+
+	for(i=0;i<numberToAdd;i++)
+	{
+		list_add_to_front(&list, &i);
+	}
+
+	list_delete_from_front(&list);
+
+	list_get_front(&list, &frontValue);
+	assert(list_get_length(&list) == numberToAdd - 1);
+	assert(frontValue != valueToAdd);
+
+	list_free(&list);
+
+	printf("Test list delete from front to non empty int SUCCEEDED\n");
+
+}
+
+void test_list_delete_from_back_to_empty_int(){
+
+	list list;
+
+	int toAdd;
+
+	toAdd=VALUE_TO_ADD;
+
+	list_new(&list, sizeof(int), NULL);
+
+	list_add_to_front(&list, &toAdd);
+
+	list_delete_from_back(&list);
+
+	assert(list_get_length(&list) == 0);
+	assert(list.front == NULL);
+	assert(list.back == NULL);
+
+	list_free(&list);
+
+	printf("Test list delete from back to empty int SUCCEEDED\n");
+}
+
+void test_list_delete_from_back_to_non_empty_int(){
+	
+	list list;
+	int valueToAdd, i, backValue, numberToAdd;
+
+	valueToAdd = VALUE_TO_ADD;
+	numberToAdd = AMOUNT_TO_ADD;
+
+	list_new(&list, sizeof(int), NULL);
+
+	for(i=0;i<numberToAdd;i++)
+	{
+		list_add_to_front(&list, &i);
+	}
+
+	list_delete_from_back(&list);
+
+	list_get_back(&list, &backValue);
+	assert(list_get_length(&list) == numberToAdd - 1);
+	assert(backValue != valueToAdd);
+
+	list_free(&list);
+
+	printf("Test list delete from back to non empty int SUCCEEDED\n");
 }
 
 void test_list_new_str(){
@@ -201,52 +401,21 @@ void test_list_add_to_front_empty_str(){
 	printf("Test list add to front from empty str SUCCEEDED\n");
 }
 
-
-void test_list_ptr_not_same()
-{
-	list list;
-
-	char* toAdd, *frontValue, *backValue, *posValue;
-
-	toAdd="test";
-
-	list_new(&list, sizeof(char*), NULL);
-
-	list_add_to_front(&list, &toAdd);
-	list_add_to_front(&list, &toAdd);
-
-	list_get_front(&list, &frontValue);
-	list_get_back(&list, &backValue);
-	list_get_at_position(&list, &posValue, 0);
-	list_get_at_position(&list, &posValue, list_get_length(&list) - 1);
-
-printf("F:%p B:%p P:%p ToAdd: %p\n", frontValue, backValue, posValue, toAdd);
-
-	assert(frontValue != toAdd);
-	assert(backValue != toAdd);
-
-	assert(frontValue != backValue);
-	assert(backValue == posValue);
-
-
-	assert(strcmp(backValue, toAdd) == 0);
-	assert(strcmp(posValue, toAdd) == 0);
-	assert(list_get_length(&list) == 1);
-
-	list_free(&list);
-
-	printf("Test list ptr not same SUCCEEDED\n");
-}
-
-
 void test_all_list_scenarios() {
 	test_list_new_int();
+	test_list_free();
 	test_list_add_to_front_empty_int();
 	test_list_get_length();
 	test_list_add_to_front_non_empty_int();
 	test_list_add_to_back_empty_int();
 	test_list_add_to_back_non_empty_int();
-	test_list_add_to_front_empty_str();
+	test_list_add_to_pos_zero_empty_int();
+	test_list_add_to_pos_length_empty_int();
+	test_list_add_to_pos_non_empty_int();
+	test_list_delete_from_front_to_empty_int();
+	test_list_delete_from_front_to_non_empty_int();
+	test_list_delete_from_back_to_empty_int();
+	test_list_delete_from_back_to_non_empty_int();
 	test_list_new_str();
-	test_list_ptr_not_same();
+	test_list_add_to_front_empty_str();
 }
